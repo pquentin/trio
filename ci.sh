5dfd4c17537955ba0cf1ce51fb9c57691498de75
Quentin Pradet <quentin.pradet@gmail.com>
2020-02-07 11:41:46 +0400

    Simplify remaining files

--- ci.sh
+++ ci.sh
@@ -1,23 +1,6 @@
 #!/bin/bash
 
-set -ex -o pipefail
+set -o errexit
 
-# Log some general info about the environment
-env | sort
-
-################################################################
-# We have a Python environment!
-################################################################
-
-python -c "import sys, struct, ssl, sqlite3; print('#' * 70); print('python:', sys.version); print('version_info:', sys.version_info); print('bits:', struct.calcsize('P') * 8); print('openssl:', ssl.OPENSSL_VERSION, ssl.OPENSSL_VERSION_INFO); print('sqlite:', sqlite3.version, sqlite3.sqlite_version); print('#' * 70)"
-
-python -m pip install -U pip setuptools wheel
-python -m pip --version
-
-# python setup.py sdist --formats=zip
-# python -m pip install dist/*.zip
-
-# Actual tests
 python -m pip install coverage==5.0.3 pytest-cov==2.8.1 pytest==5.3.5
-
 pytest -W error -r a repro --cov=repro --cov-config=.coveragerc --verbose
