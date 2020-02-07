cd932fbfc03501007c35889870a252f175b8a786
Quentin Pradet <quentin.pradet@gmail.com>
2020-02-07 11:16:44 +0400

    Inline FdHolder

--- trio/tests/test_unix_pipes.py
+++ trio/tests/test_unix_pipes.py
@@ -1,8 +1,51 @@
-import pytest
-
-from .._unix_pipes import FdStream
+import os
 
 
-async def test_pipe_errors():
-    fd = FdStream(0)
-    fd._fd_holder._raw_close()
+class _FdHolder:
+    # This class holds onto a raw file descriptor, in non-blocking mode, and
+    # is responsible for managing its lifecycle. In particular, it's
+    # responsible for making sure it gets closed, and also for tracking
+    # whether it's been closed.
+    #
+    # The way we track closure is to set the .fd field to -1, discarding the
+    # original value. You might think that this is a strange idea, since it
+    # overloads the same field to do two different things. Wouldn't it be more
+    # natural to have a dedicated .closed field? But that would be more
+    # error-prone. Fds are represented by small integers, and once an fd is
+    # closed, its integer value may be reused immediately. If we accidentally
+    # used the old fd after being closed, we might end up doing something to
+    # another unrelated fd that happened to get assigned the same integer
+    # value. By throwing away the integer value immediately, it becomes
+    # impossible to make this mistake – we'll just get an EBADF.
+    #
+    # (This trick was copied from the stdlib socket module.)
+    def __init__(self, fd: int):
+        # make sure self.fd is always initialized to *something*, because even
+        # if we error out here then __del__ will run and access it.
+        self.fd = -1
+        if not isinstance(fd, int):
+            raise TypeError("file descriptor must be an int")
+        self.fd = fd
+        # Store original state, and ensure non-blocking mode is enabled
+        self._original_is_blocking = os.get_blocking(fd)
+        os.set_blocking(fd, False)
+
+    def _raw_close(self):
+        # This doesn't assume it's in a Trio context, so it can be called from
+        # __del__. You should never call it from Trio context, because it
+        # skips calling notify_fd_close. But from __del__, skipping that is
+        # OK, because notify_fd_close just wakes up other tasks that are
+        # waiting on this fd, and those tasks hold a reference to this object.
+        # So if __del__ is being called, we know there aren't any tasks that
+        # need to be woken.
+        if self.fd == -1:
+            return
+        fd = self.fd
+        self.fd = -1
+        os.set_blocking(fd, self._original_is_blocking)
+        os.close(fd)
+
+
+def test_pipe_errors():
+    fd_holder = _FdHolder(0)
+    fd_holder._raw_close()
