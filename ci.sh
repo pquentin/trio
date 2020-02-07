b476140435624a72185aa55902e8baa21667e079
Quentin Pradet <quentin.pradet@gmail.com>
2020-02-07 12:40:20 +0400

    Remove failed reproduction attempt

--- ci.sh
+++ ci.sh
@@ -3,5 +3,4 @@
 set -o errexit
 
 python -m pip install coverage==5.0.3 pytest-cov==2.8.1 pytest==5.3.5
-python t.py
 pytest --cov=.
--- t.py
+++
@@ -1,7 +0,0 @@
-import os
-
-import sqlite3
-
-con = sqlite3.connect("db.sqlite", check_same_thread=False)
-os.close(0)
-con.close()
