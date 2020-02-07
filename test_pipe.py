a31808bbb6c31732d1a0dffc40f76e05e851350d
Quentin Pradet <quentin.pradet@gmail.com>
2020-02-07 11:52:02 +0400

    Stop using set_blocking

--- test_pipe.py
+++ test_pipe.py
@@ -2,9 +2,4 @@ import os
 
 def test_pipe_errors():
     fd = 0
-    # Store original state, and ensure non-blocking mode is enabled
-    _original_is_blocking = os.get_blocking(fd)
-    os.set_blocking(fd, False)
-
-    os.set_blocking(fd, _original_is_blocking)
     os.close(fd)
