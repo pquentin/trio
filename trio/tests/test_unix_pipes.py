7efb13cce1498b33c843fc5fbf268e1e661998b4
Quentin Pradet <quentin.pradet@gmail.com>
2020-02-07 10:02:20 +0400

    Remove top half of tests (2 remaining)

--- trio/tests/test_unix_pipes.py
+++ trio/tests/test_unix_pipes.py
@@ -50,50 +50,6 @@ async def make_clogged_pipe():
     return s, r
 
 
-async def test_send_pipe():
-    r, w = os.pipe()
-    async with FdStream(w) as send:
-        assert send.fileno() == w
-        await send.send_all(b"123")
-        assert (os.read(r, 8)) == b"123"
-
-        os.close(r)
-
-
-async def test_receive_pipe():
-    r, w = os.pipe()
-    async with FdStream(r) as recv:
-        assert (recv.fileno()) == r
-        os.write(w, b"123")
-        assert (await recv.receive_some(8)) == b"123"
-
-        os.close(w)
-
-
-async def test_pipes_combined():
-    write, read = await make_pipe()
-    count = 2**20
-
-    async def sender():
-        big = bytearray(count)
-        await write.send_all(big)
-
-    async def reader():
-        await wait_all_tasks_blocked()
-        received = 0
-        while received < count:
-            received += len(await read.receive_some(4096))
-
-        assert received == count
-
-    async with _core.open_nursery() as n:
-        n.start_soon(sender)
-        n.start_soon(reader)
-
-    await read.aclose()
-    await write.aclose()
-
-
 async def test_pipe_errors():
     with pytest.raises(TypeError):
         FdStream(None)
